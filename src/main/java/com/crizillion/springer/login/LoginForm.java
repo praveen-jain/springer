eba98a0d4868c1c4423b6b57405b7ab17d4f06b3
Praveen <Praveen@PRAVEENJ>
2019-06-16 20:47:13 +0530

    Setup of the following:
    1. JSP
    2. Static resources

---
+++ src/main/java/com/crizillion/springer/login/LoginForm.java
@@ -0,0 +1,33 @@
+package com.crizillion.springer.login;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class LoginForm {
+	
+	@NotBlank(message = "E-Mail must not be empty.")
+	@Email(message = "Please enter a valid e-mail address.")
+	private String email;
+
+	@NotBlank(message = "Password must not be empty.")
+	private String password;
+
+	public LoginForm() {
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ src/main/java/com/crizillion/springer/login/LoginHandler.java
@@ -0,0 +1,42 @@
+package com.crizillion.springer.login;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginHandler {
+	private static final String LOGIN_VIEW = "login";
+	private static final String LOGOUT_VIEW = "logout";
+	// controller body
+
+	@GetMapping("/login")
+	public String showLoginView(Model model) {
+		model.addAttribute("loginForm", new LoginForm());
+		return LOGIN_VIEW;
+	}
+
+	@PostMapping("/login")
+	public String login(@Valid @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpSession session) {
+		if (!bindingResult.hasErrors()) {
+			final boolean authenticated = "abc@test.com".equals(form.getEmail());
+			final String loginStatus = authenticated ? "OK" : "FAILED";
+			session.setAttribute("loginStatus", loginStatus);
+		}
+		return LOGIN_VIEW;
+	}
+
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("loginStatus");
+		return LOGOUT_VIEW;
+	}
+}
